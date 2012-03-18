63e272c13c366da1bce4dc6b994bf8f40a7054dc
Jindra Basek <basekjin@fit.cvut.cz>
2012-03-19 00:37:06 +0100

    Real metada and first release of snapshot creation

--- models/model.js
+++ models/model.js
@@ -2,34 +2,40 @@
 var diff = 24*60*60*1000,
 	num = 50,
 	t = (new Date).getTime() - 50*diff;
-	snapshots = [],
 	updateIntervalMinutes = 1,
 	dbAccess = null;
 
 // constructor
 function Model(iDbAccess) {
 	dbAccess = iDbAccess;
-	for (var i = 0; i < num; i+=1, t+=diff) {
-		snapshots.push({created: new Date(t), node: Math.floor(Math.random()*300), edge: Math.floor(Math.random()*600)});
-	}
 }
 
 Model.prototype = {
-	lastUpdate: function() {
-		var time = new Date(t);
+	lastUpdate: function(callback) {
+		var time = null;
 		dbAccess.getDb().collection('appData', function(err, collection) {
 	        collection.findOne({name:'lastUpdate'}, function(err, item) {
 	        	if (item != null) {
 	        		time = item.value;
 	        	}
+	        	callback(time);
 	        });
 	    });
-		return new Date(time);
 	},
 	updateInterval: function() { return updateIntervalMinutes; /* 1 day = 1440 minutes */ },
 	updateIntervalMs: function() { return updateIntervalMinutes * 60 * 1000;},
-	snapshots: function() {
-		return snapshots;
+	snapshots: function(callback) {
+		var snapshots = [];
+		dbAccess.getDb().collection('snapshots', function(err, collection) {
+			collection.find().toArray(function(err, items) {
+	        	if (items != null) {
+	        		for(var i in items) {
+	        			snapshots.push({created: new Date(items[i].created), node: items[i].node, edge: items[i].edge});
+	        		}
+	        	}
+    			callback(snapshots);
+	        });
+	    });
 	}
 };
 

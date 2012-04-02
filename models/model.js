923d5dc343abf8857b1259e08517651b8797adc1
Jan Marek <mail@janmarek.net>
2012-04-02 19:08:37 +0200

    refactoring

--- models/model.js
+++ models/model.js
@@ -1,20 +1,14 @@
-
-var diff = 24*60*60*1000,
-	num = 50,
-	t = (new Date).getTime() - 50*diff;
-	updateIntervalMinutes = 360,
-	dbAccess = null;
-
 // constructor
-function Model(iDbAccess) {
-	dbAccess = iDbAccess;
+function Model(dbAccess) {
+	this.dbAccess = dbAccess;
+	this.updateIntervalMinutes = 360;
 }
 
 Model.prototype = {
 	lastUpdate: function(callback) {
 		var time = null;
-		dbAccess.getDb().collection('appData', function(err, collection) {
-	        collection.findOne({name:'lastUpdate'}, function(err, item) {
+		this.dbAccess.getDb().collection('appData', function(err, collection) {
+	        collection.findOne({name: 'lastUpdate'}, function(err, item) {
 	        	if (item != null) {
 	        		time = item.value;
 	        	}
@@ -22,15 +16,25 @@ Model.prototype = {
 	        });
 	    });
 	},
-	updateInterval: function() { return updateIntervalMinutes; /* 1 day = 1440 minutes */ },
-	updateIntervalMs: function() { return updateIntervalMinutes * 60 * 1000;},
-	maxMeetingsInSnapshot: function() { return 5;},
+
+	updateInterval: function() {
+		return this.updateIntervalMinutes; /* 1 day = 1440 minutes */
+	},
+
+	updateIntervalMs: function() {
+		return this.updateIntervalMinutes * 60 * 1000;
+	},
+
+	maxMeetingsInSnapshot: function() {
+		return 5;
+	},
+
 	snapshots: function(callback) {
 		var snapshots = [];
-		dbAccess.getDb().collection('snapshots', function(err, collection) {
+		this.dbAccess.getDb().collection('snapshots', function(err, collection) {
 			collection.find().toArray(function(err, items) {
 	        	if (items != null) {
-	        		for(var i in items) {
+	        		for (var i in items) {
 	        			snapshots.push({created: new Date(items[i].created), node: items[i].node, edge: items[i].edge});
 	        		}
 	        	}
